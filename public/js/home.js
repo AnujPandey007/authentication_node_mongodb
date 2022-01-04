8753e6b547104794f222a259ba83c973b0c51fb0
anuj <panuj330@gmail.com>
2022-01-04 11:44:34 +0530

    complete app

---
+++ public/js/home.js
@@ -0,0 +1,30 @@
+
+getUserData();
+
+async function getUserData() {
+    fetch("http://authenticationmongoose.herokuapp.com/getUsers/").then(function (response) {
+        return response.json();
+    }).then(function (data) {
+        appendData(data);
+    }).catch(function (err) {
+        console.log(err);
+    });
+}
+
+function appendData(data) {
+    var mainContainer = document.getElementById("userCard");
+    for (var i = 0; i < data.length; i++) {
+      var div = document.createElement("div");
+      div.innerHTML = `<div class="card">
+                        <span style="--i:0;"></span>
+                        <span style="--i:1;"></span>
+                        <span style="--i:2;"></span>
+                        <span style="--i:3;"></span>
+                        <div class="cardBox">
+                            <div class="quote"><h2>${data[i].userName}</h2> <br> <br> <div>${data[i].userEmail}</div> </div>
+                            <div class="glass"><h2>Quoted By <i><br> Lord Krishna</i> </h2></div>
+                        </div>
+                    </div>`
+      mainContainer.appendChild(div);
+    }
+}
--- public/js/login.js
+++ public/js/login.js
@@ -1,36 +1,3 @@
-// alert("Hi, Welcome");
-
-// let getPromptValue = prompt("What is your name?", "Guest");
-// console.log(getPromptValue);
-
-// let deleteMessage = confirm("Do you really want to delete this post?");
-// console.log(deleteMessage);
-
-// getApi();
-
-// import express from 'express';
-
-// const app = express();
-
-// app.post('/login', async(req, res)=>{
-//     let _data = {
-//         userName: "qwe",
-//         userAge: 20,
-//         userEmail: "req.body.userEmail",
-//         userPassword: "req.body.userPassword",
-//         isMale: true
-//     }
-      
-//     fetch('https://jsonplaceholder.typicode.com/posts', {
-//         method: "POST",
-//         body: JSON.stringify(_data),
-//         headers: {"Content-type": "application/json; charset=UTF-8"}
-//     })
-//     .then(response => response.json()) 
-//     .then(json => console.log(json))
-//     .catch(err => console.log(err));
-// });
-
 
 async function createUser() {
     let userName = document.getElementById('Uname').value;
@@ -54,7 +21,7 @@ async function createUser() {
         headers: {"Content-type": "application/json; charset=UTF-8"}
     })
     .then(response => response.json()) 
-    .then(json => /*console.log(json)*/window.location.replace("https://authenticationmongoose.herokuapp.com/about"))
+    .then(json => /*console.log(json)*/window.location.replace("https://authenticationmongoose.herokuapp.com/homePage"))
     .catch(err => console.log(err));
 }
 
