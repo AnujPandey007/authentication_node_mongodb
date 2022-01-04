e78e65a4deb2af0a3f9533150376bf0c63842b43
anuj <panuj330@gmail.com>
2022-01-04 10:23:33 +0530

    make it better

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/js', express.static(__dirname + 'public/js'));
 app.use('/img', express.static(__dirname + 'public/img'));
 
+
 // Set Views
 app.set('views', './views');
 app.set('view engine', 'ejs');
